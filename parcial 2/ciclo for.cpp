8611881571fe1dc69cb325524f9f48c0f999b93e
Alexander Ruiz Cota <82423439+alexruiz24021883@users.noreply.github.com>
2024-11-03 22:49:34 -0700

    Add files via upload

---
+++ parcial 2/ciclo for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int x;
+x = 1;
+for(x=1;x<=10;x++)
+    
+cout<<x<<endl;
+
+
+
+
+    return 0;
+}
---
+++ parcial 2/ciclo if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int numero, dato = 5;
+
+cout<<"ingrese un numero"<<endl;
+cin>>numero;
+
+if( numero == dato){
+    cout<<"el numero es 5"<<endl;
+}
+
+    else {
+        cout<<"el numero es diferente de 5"<<endl;
+}
+
+
+
+
+    return 0;
+}
